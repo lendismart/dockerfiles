8c5f8000d91a111e8b45ed1b7e7e04f9f648dd9c
jroy <jorgeroy@gmail.com>
2020-07-17 08:30:16 +0200

    cert file perm

--- postgres-backup-s3/backup.sh
+++ postgres-backup-s3/backup.sh
@@ -32,6 +32,7 @@ export AWS_DEFAULT_REGION=$S3_REGION
 
 export PGPASSWORD=$POSTGRES_PASSWORD
 
+chmod 0600 /opt/cert/jdbc/client-key.pem
 echo "Creating dump with ssl2..."
 
 pg_dump --encoding=UTF-8 --no-privileges --no-owner  "${POSTGRES_EXTRA_OPTS}" | gzip | openssl enc -aes-256-cbc -e -pass pass:$ENCRYPTION_PASS > dump.sql.gz.enc
